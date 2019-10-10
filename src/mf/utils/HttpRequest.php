ebdb722c570853a314402b1f60e47da821fc4253
richierm <maxime.richier9@etu.univ-lorraine.fr>
2019-10-10 11:56:18 +0200

    Check existence properly in HttpRequest

--- src/mf/utils/HttpRequest.php
+++ src/mf/utils/HttpRequest.php
@@ -6,12 +6,18 @@ class HttpRequest extends AbstractHttpRequest
 {
     public function __construct()
     {
-        $this->script_name = $_SERVER["SCRIPT_NAME"];
+        if (isset($_SERVER["SCRIPT_NAME"])) {
+            $this->script_name = $_SERVER["SCRIPT_NAME"];
+        }
         if (isset($_SERVER["PATH_INFO"])) {
             $this->path_info = $_SERVER["PATH_INFO"];
         }
-        $this->root = dirname($_SERVER['SCRIPT_NAME']);
-        $this->method = $_SERVER["REQUEST_METHOD"];
+        if (isset($_SERVER["SCRIPT_NAME"])) {
+            $this->root = dirname($_SERVER['SCRIPT_NAME']);
+        }
+        if (isset($_SERVER["REQUEST_METHOD"])) {
+            $this->method = $_SERVER["REQUEST_METHOD"];
+        }
         $this->get = $_GET;
         $this->post = $_POST;
     }
