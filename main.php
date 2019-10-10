a3a231145967944890141555c2abd7cf1c3a40e8
richierm <maxime.richier9@etu.univ-lorraine.fr>
2019-10-10 11:57:37 +0200

    Implemented basic display functionnalities controller
    Tweeter controller can display all tweets, all tweets from user, one specific tweet

--- main.php
+++ main.php
@@ -1,8 +1,11 @@
 <?php
 
+use mf\router\Router;
 use mf\utils\ClassLoader;
 use tweeterapp\model\Tweet;
 use tweeterapp\model\User;
+
+!defined('WEBSITE_PATH_PREFIX') && define('WEBSITE_PATH_PREFIX', '/PhpstormProjects/Mini_Projet_Php_Tweeter/Tweeter/main.php');
 
 require_once "src" . DIRECTORY_SEPARATOR . "mf" . DIRECTORY_SEPARATOR . "utils" . DIRECTORY_SEPARATOR . "ClassLoader.php";
 new ClassLoader('src'); // Auto-register
@@ -23,107 +26,30 @@ $db->addConnection($config);
 $db->setAsGlobal();
 $db->bootEloquent();
 
+/* test controller
+$ctrl = new tweeterapp\control\TweeterController();
+echo $ctrl->viewHome();*/
 
-//SELECT * FROM
-function getAllUsers()
-{
-    $allUsers = User::select()->get();
-    foreach ($allUsers as $v) {
-        echo "Id: $v->id, name: $v->fullname\n";
-    }
-}
+//test router
+$router = new \mf\router\Router();
 
-function getAllTweets()
-{
-    $allTweets = User::select()->get();
-    foreach ($allTweets as $v) {
-        echo "Id: $v->id, text: $v->text\n";
-    }
-}
+$router->addRoute('maison',
+    '/home/',
+    '\tweeterapp\control\TweeterController',
+    'viewHome');
 
+$router->setDefaultRoute('/home/');
 
-//SELECT with conditions
-function getAllTweetsSortedByEdit()
-{
-    $allTweetsSortedByEdit = Tweet::select()->orderBy('updated_at')->get();
-    foreach ($allTweetsSortedByEdit as $v) {
-        echo "Edit: $v->updated_at, text: $v->text\n";
-    }
-}
+/* Après exécution de cette instruction, l'attribut statique $routes et
+   $aliases de la classe Router auront les valeurs suivantes: */
 
-function getAllTweetsWPositiveScore()
-{
-    $allTweetsWPositiveScore = Tweet::select()->where('score', '>', 0)->get();
-    foreach ($allTweetsWPositiveScore as $v) {
-        echo "Id: $v->id, text: $v->text\n";
-    }
-}
+//Test router::run
+$router = new \mf\router\Router();
 
+$router->addRoute('home', '/home/', '\tweeterapp\control\TweeterController', 'viewHome');
+$router->addRoute('tweet', '/tweet/', '\tweeterapp\control\TweeterController', 'viewTweet');
+$router->addRoute('userTweets', '/user/', '\tweeterapp\control\TweeterController', 'viewUserTweets');
 
-//Creating new entries
-function createTweet()
-{
-    $t = new Tweet();
-    $t->text = "The Do Nothing Democrats are Con Artists, only looking to hurt the Republican Party and President. Their total focus is 2020, nothing more, and nothing less. The good news is that WE WILL WIN!!!!";
-    $t->author = 1;
-    //$t->save();
-}
+$router->setDefaultRoute('/home/');
 
-function createUser()
-{
-    $u = new User();
-    $u->fullname = "Barrack Obama";
-    $u->username = "obamaofficial";
-    $u->level = 100;
-//    $u->save();
-}
-
-
-//Association 1->*
-function getTweetAuthor()
-{
-    $tweet = Tweet::select()->first();
-    echo $tweet->author()->first();
-}
-
-function getAllTweetFromAuthor()
-{
-    $user = User::select()->where('id', '=', 1)->first();
-    foreach ($user->tweets()->get() as $v) {
-        echo "Id: $v->id, text: $v->text\n";
-    }
-}
-
-
-//Association *->*
-function getUsersWhoLiked()
-{
-    $tweet = Tweet::select()->where('id', '=', 63)->first();
-    foreach ($tweet->likedBy()->get() as $v) {
-        echo "$v->fullname\n";
-    }
-}
-
-function getAllLikedTweets()
-{
-    $user = User::select()->where('id', '=', 10)->first();
-    foreach ($user->liked()->get() as $v) {
-        echo "$v->text\n";
-    }
-}
-
-function getAllFollowers()
-{
-    $followee = User::select()->where('id', '=', 9)->first();
-    foreach ($followee->followedBy()->get() as $v) {
-        echo "$v->fullname\n";
-    }
-}
-
-function getAllFollowed()
-{
-    $follower = User::select()->where('id', '=', 10)->first();
-    foreach ($follower->follows()->get() as $v) {
-        echo "$v->fullname\n";
-    }
-}
+$router->run();
---
+++ src/tweeterapp/control/TweeterController.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace tweeterapp\control;
+
+/* Classe TweeterController :
+ *
+ * RÃ©alise les algorithmes des fonctionnalitÃ©s suivantes:
+ *
+ *  - afficher la liste des Tweets
+ *  - afficher un Tweet
+ *  - afficher les tweet d'un utilisateur
+ *  - afficher la le formulaire pour poster un Tweet
+ *  - afficher la liste des utilisateurs suivis
+ *  - Ã©valuer un Tweet
+ *  - suivre un utilisateur
+ *
+ */
+
+use mf\router\Router;
+use tweeterapp\model\Tweet;
+use tweeterapp\model\User;
+
+class TweeterController extends \mf\control\AbstractController {
+
+
+    /* Constructeur :
+     *
+     * Appelle le constructeur parent
+     *
+     * c.f. la classe \mf\control\AbstractController
+     *
+     */
+
+    public function __construct(){
+        parent::__construct();
+    }
+
+
+    /* MÃ©thode viewHome :
+     *
+     * RÃ©alise la fonctionnalitÃ© : afficher la liste de Tweet
+     *
+     */
+
+    public function viewHome(){
+
+        /* Algorithme :
+         *
+         *  1 RÃ©cupÃ©rer tout les tweet en utilisant le modÃ¨le Tweet
+         *  2 Parcourir le rÃ©sultat
+         *      afficher le text du tweet, l'auteur et la date de crÃ©ation
+         *  3 Retourner un block HTML qui met en forme la liste
+         *
+         */
+
+        foreach(Tweet::select()->get() as $tweet) {
+            echo "$tweet->text | ".$tweet->author()->first()->fullname.", $tweet->created_at\n";
+        }
+    }
+
+
+    /* MÃ©thode viewTweet :
+     *
+     * RÃ©alise la fonctionnalitÃ© afficher un Tweet
+     *
+     */
+
+    public function viewTweet(){
+
+        /* Algorithme :
+         *
+         *  1 L'identifiant du Tweet en question est passÃ© en paramÃ¨tre (id)
+         *      d'une requÃªte GET
+         *  2 RÃ©cupÃ©rer le Tweet depuis le modÃ¨le Tweet
+         *  3 Afficher toutes les informations du tweet
+         *      (text, auteur, date, score)
+         *  4 Retourner un block HTML qui met en forme le Tweet
+         *
+         *  Erreurs possibles : (*** Ã  implanter ultÃ©rieurement ***)
+         *    - pas de paramÃ¨tre dans la requÃªte
+         *    - le paramÃ¨tre passÃ© ne correspond pas a un identifiant existant
+         *    - le paramÃ¨tre passÃ© n'est pas un entier
+         *
+         */
+
+        if (isset($this->request->get["id"])) {
+            $id = $this->request->get["id"];
+            $tweet = Tweet::select()->where('id', '=', $id)->first();
+            echo '<p>$tweet->text | <a href="'
+                    .Router::urlFor("userTweets", array("id"=>$tweet->author)) .'">'
+                    .$tweet->author()->first()->fullname.
+                '</a>'
+                .", $tweet->created_at : $tweet->score likes.</p>";
+        }
+
+        echo '';
+    }
+
+
+    /* MÃ©thode viewUserTweets :
+     *
+     * RÃ©alise la fonctionnalitÃ© afficher les tweet d'un utilisateur
+     *
+     */
+
+    public function viewUserTweets(){
+
+        /*
+         *
+         *  1 L'identifiant de l'utilisateur en question est passÃ© en
+         *      paramÃ¨tre (id) d'une requÃªte GET
+         *  2 RÃ©cupÃ©rer l'utilisateur et ses Tweets depuis le modÃ¨le
+         *      Tweet et User
+         *  3 Afficher les informations de l'utilisateur
+         *      (non, login, nombre de suiveurs)
+         *  4 Afficher ses Tweets (text, auteur, date)
+         *  5 Retourner un block HTML qui met en forme la liste
+         *
+         *  Erreurs possibles : (*** Ã  implanter ultÃ©rieurement ***)
+         *    - pas de paramÃ¨tre dans la requÃªte
+         *    - le paramÃ¨tre passÃ© ne correspond pas a un identifiant existant
+         *    - le paramÃ¨tre passÃ© n'est pas un entier
+         *
+         */
+
+        if (isset($this->request->get["id"])) {
+            $id = $this->request->get["id"];
+            $user = User::select()->where('id', '=', $id)->first();
+            echo "$user->fullname @$user->username | $user->followers followers.<br>\n";
+            $tweets = $user->tweets()->get();
+            foreach ($tweets as $tweet) {
+                echo "$tweet->text | ".$tweet->author()->first()->fullname.", $tweet->created_at : $tweet->score likes.<br>\n";
+            }
+        }
+
+    }
+}
